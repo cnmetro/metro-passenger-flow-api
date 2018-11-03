2869d6917a00f0e599ea89f93a1aea4cbd67b842
sinchang <sinchangwen@gmail.com>
2018-11-03 09:40:53 +0800

    Encode telegram message

--- telegram.js
+++ telegram.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 module.exports = text => {
   const url = `https://api.telegram.org/bot${
     process.env.BOT_TOKEN
-  }/sendMessage?chat_id=${process.env.CHAT_ID}&text=${text}`
+  }/sendMessage?chat_id=${process.env.CHAT_ID}&text=${encodeURIComponent(text)}`
 
   axios.get(url).catch(err => {
     console.log(err)
